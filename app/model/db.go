436192e04e9203496aabf42eb3de0d096a62c7d7
Julien-FRA <julien.francois@hetic.net>
2023-03-02 13:00:34 +0100

    modif db

--- app/model/db.go
+++ app/model/db.go
@@ -3,9 +3,9 @@ package model
 import (
 	"database/sql"
 	"fmt"
-	"os"
+	// "os"
 
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 
 	_ "github.com/lib/pq"
 )
@@ -24,23 +24,28 @@ type connection struct {
 }
 
 func Init() {
-	err := godotenv.Load("config/.env")
+	// err := godotenv.Load("config/.env")
 
-	if err != nil {
-		fmt.Printf("Error loading .env file: %s\n", err.Error())
-		return
-	}
+	// if err != nil {
+	// 	fmt.Printf("Error loading .env file: %s\n", err.Error())
+	// 	return
+	// }
 
 	connInfo := connection{
-		Host:     os.Getenv("POSTGRES_URL"),
-		Port:     os.Getenv("POSTGRES_PORT"),
-		User:     os.Getenv("POSTGRES_USER"),
-		Password: os.Getenv("POSTGRES_PASSWORD"),
-		DBName:   os.Getenv("POSTGRES_DB"),
+		// Host:     os.Getenv("POSTGRES_URL"),
+		// Port:     os.Getenv("POSTGRES_PORT"),
+		// User:     os.Getenv("POSTGRES_USER"),
+		// Password: os.Getenv("POSTGRES_PASSWORD"),
+		// DBName:   os.Getenv("POSTGRES_DB"),
+		Host:     "postgres",
+		Port:     "5432",
+		User:     "dev",
+		Password: "dev-password",
+		DBName:   "devdb",
 	}
 
 	// try to open our postgresql connection with our connection info
-	db, err = sql.Open("postgres", connToString(connInfo))
+	db, err := sql.Open("postgres", connToString(connInfo))
 	if err != nil {
 		fmt.Printf("Error connecting to the DB: %s\n", err.Error())
 		return
